1c9ea0fa57ad1584f38335db8cb6c7279ceceb0f
guodewei <guodewei_work@163.com>
2018-02-06 10:36:03 +0800

    appkey提交

--- app/src/main/java/com/example/tcl/customview/ThreeRecyclerViewActivity.java
+++ app/src/main/java/com/example/tcl/customview/ThreeRecyclerViewActivity.java
@@ -59,6 +59,7 @@ public class ThreeRecyclerViewActivity extends BaseActivity {
             }
         });
         mRecyclerView.setLayoutManager(vLayoutManager);
+
         mRecyclerView.setAdapter(mAdapter);
     }
 
